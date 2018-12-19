a71e0e0b963902f9b1e2f2b7f5d486b132c7c824
hcmf-wice <hcmf.wice@gmail.com>
2018-12-19 20:48:56 +0100

    - IIFE

--- scripts/content.js
+++ scripts/content.js
@@ -2,7 +2,54 @@
 
 console.log('content.js');
 
-const initPage = () => {
+(() => {
+  const updatePage = async callback => {
+    const settings = await getSettings();
+    getRows().forEach(row => updateRow(row, settings));
+    if (callback) callback();
+  };
+
+  const updateRow = (row, settings) => {
+    const img = row.firstElementChild.firstElementChild;
+    const isBlacklisted = settings.blacklist.find(o => o.id === img.dataset.id);
+    img.src = isBlacklisted ? SHOW_IMG_SRC : HIDE_IMG_SRC;
+    img.dataset.action = isBlacklisted ? BUTTON_ACTION.REMOVE_FROM_BLACKLIST : BUTTON_ACTION.ADD_TO_BLACKLIST;
+    if (isBlacklisted) addEventsListener(row, ['mouseover', 'mouseout'], mouseHandler);
+    else removeEventsListener(row, ['mouseover', 'mouseout'], mouseHandler);
+    row.style.opacity = isBlacklisted ? '0.2' : '1.0';
+    row.hidden = isBlacklisted ? settings.displayStyle === DISPLAY_STYLE.HIDE : false;
+    row.nextElementSibling.hidden = isBlacklisted ? settings.displayStyle === DISPLAY_STYLE.HIDE : false;
+  };
+
+  const id = idStr => idStr.replace('sid=', '').replace('fid=', '');
+  const type = isTv => isTv ? TYPE.TV : TYPE.FILM;
+  const cleanTitle = (isTv, title) => isTv ? cleanTvTitle(title) : cleanFilmTitle(title);
+  const buttonTd = (id, type, title) => td({style: 'width: 20px;'}, img({dataset: {id, type, title}, style: 'cursor: pointer', eventListener: ['click', actionHandler]}));
+  const cleanTvTitle = title => {
+    const match = title.match(/.*? - \d+?x/) || title.match(/.*?[ (]Season \d/);
+    return match ? match[0].replace(/ - \d+?x/, '').replace(/ *[ (]Season \d/, '') : title;
+  };
+  const cleanFilmTitle = title => {
+    const match = title.match(/.*?\(\d{4}\)/);
+    return match ? match[0] : title;
+  };
+
+  const mouseHandler = e => e.currentTarget.style.opacity = (e.type === 'mouseout' ? '0.2' : '1.0');
+
+  const actionHandler = async event => {
+    const {id, type, title, action} = event.currentTarget.dataset;
+    const settings = await getSettings();
+    if (action === BUTTON_ACTION.REMOVE_FROM_BLACKLIST) {
+      settings.blacklist = settings.blacklist.filter(item => item.id !== id);
+    } else if (!settings.blacklist.find(item => item.id === id)) {
+      settings.blacklist.unshift({id, title, type});
+    }
+    await saveSettings(settings);
+    updatePage();
+  };
+
+  const getRows = () => [...document.querySelectorAll('[id="vilagit"]')];
+
   const headRow = document.getElementsByClassName('result')[0].children[0].children[0];
   headRow.insertBefore(th({style: 'width: 20px;'}), headRow.firstChild);
 
@@ -14,53 +61,4 @@ const initPage = () => {
 
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {if (request.command === COMMAND.UPDATE_PAGE) updatePage(sendResponse)});
   updatePage();
-};
-
-const updatePage = async callback => {
-  const settings = await getSettings();
-  getRows().forEach(row => updateRow(row, settings));
-  if (callback) callback();
-};
-
-const updateRow = (row, settings) => {
-  const img = row.firstElementChild.firstElementChild;
-  const isBlacklisted = settings.blacklist.find(o => o.id === img.dataset.id);
-  img.src = isBlacklisted ? SHOW_IMG_SRC : HIDE_IMG_SRC;
-  img.dataset.action = isBlacklisted ? BUTTON_ACTION.REMOVE_FROM_BLACKLIST : BUTTON_ACTION.ADD_TO_BLACKLIST;
-  if (isBlacklisted) addEventsListener(row, ['mouseover', 'mouseout'], mouseHandler);
-  else removeEventsListener(row, ['mouseover', 'mouseout'], mouseHandler);
-  row.style.opacity = isBlacklisted ? '0.2' : '1.0';
-  row.hidden = isBlacklisted ? settings.displayStyle === DISPLAY_STYLE.HIDE : false;
-  row.nextElementSibling.hidden = isBlacklisted ? settings.displayStyle === DISPLAY_STYLE.HIDE : false;
-};
-
-const id = idStr => idStr.replace('sid=', '').replace('fid=', '');
-const type = isTv => isTv ? TYPE.TV : TYPE.FILM;
-const cleanTitle = (isTv, title) => isTv ? cleanTvTitle(title) : cleanFilmTitle(title);
-const buttonTd = (id, type, title) => td({style: 'width: 20px;'}, img({dataset: {id, type, title}, style: 'cursor: pointer', eventListener: ['click', actionHandler]}));
-const cleanTvTitle = title => {
-  const match = title.match(/.*? - \d+?x/) || title.match(/.*?[ (]Season \d/);
-  return match ? match[0].replace(/ - \d+?x/, '').replace(/ *[ (]Season \d/, '') : title;
-};
-const cleanFilmTitle = title => {
-  const match = title.match(/.*?\(\d{4}\)/);
-  return match ? match[0] : title;
-};
-
-const mouseHandler = e => e.currentTarget.style.opacity = (e.type === 'mouseout' ? '0.2' : '1.0');
-
-const actionHandler = async event => {
-  const {id, type, title, action} = event.currentTarget.dataset;
-  const settings = await getSettings();
-  if (action === BUTTON_ACTION.REMOVE_FROM_BLACKLIST) {
-    settings.blacklist = settings.blacklist.filter(item => item.id !== id);
-  } else if (!settings.blacklist.find(item => item.id === id)) {
-    settings.blacklist.unshift({id, title, type});
-  }
-  await saveSettings(settings);
-  updatePage();
-};
-
-const getRows = () => [...document.querySelectorAll('[id="vilagit"]')];
-
-initPage();
+})();
--- scripts/popup.js
+++ scripts/popup.js
@@ -2,72 +2,70 @@
 
 console.log('popup.js');
 
-const initPopup = () => {
+(() => {
+  const updatePopup = async () => {
+    const settings = await getSettings();
+
+    getById('displaySwitch').src = settings.displayStyle === DISPLAY_STYLE.FADE ? SWITCH_LEFT_IMG_SRC : SWITCH_RIGHT_IMG_SRC;
+    getById('displaySwitchRow').dataset.displayStyle = settings.displayStyle;
+    getById('sortingSwitch').src = settings.sorting === SORTING.DEFAULT ? SWITCH_LEFT_IMG_SRC : SWITCH_RIGHT_IMG_SRC;
+    getById('sortingSwitchRow').dataset.sorting = settings.sorting;
+    getById('blacklist').innerHTML = '';
+
+    if (settings.sorting === SORTING.TITLE) {
+      settings.blacklist.sort(titleComparator);
+    }
+
+    if (settings.blacklist.length > 0) {
+      let prevTitle = null;
+      settings.blacklist.forEach((item, index) => {
+        const titleWithoutArticle = withoutArticle(item.title);
+
+        getById('blacklist').appendChild(tr(
+            td(img({src: SHOW_IMG_SRC, style: 'cursor: pointer;', eventListener: ['click', () => removeFromBlacklist(item.id)]})),
+            td(img({src: item.type === TYPE.TV ? TV_IMG_SRC : FILM_IMG_SRC})),
+            td({
+              style: settings.sorting === SORTING.DEFAULT && index > 0 || settings.sorting === SORTING.TITLE && prevTitle && prevTitle[0] < titleWithoutArticle[0]
+                  ? 'border-top: 1px solid lightgray;'
+                  : 'border-top: 1px solid white;',
+              innerHTML: settings.sorting === SORTING.TITLE ? withGreyedOutArticle(item.title) : item.title,
+            })));
+
+        prevTitle = titleWithoutArticle;
+      });
+    } else {
+      getById('blacklist').appendChild(tr(td({style: 'color: gray; text-align: center; font-size: 80%;', innerHTML: 'Your blacklist is empty.'})));
+    }
+  };
+
+  const removeFromBlacklist = async id => {
+    const settings = await getSettings();
+    settings.blacklist = settings.blacklist.filter(item => item.id !== id);
+    await saveSettingsAndUpdatePopup(settings);
+    sendMessageToTab({command: COMMAND.UPDATE_PAGE});
+  };
+
+  const displayStyleSelectionHandler = async event => {
+    const selectorTR = event.currentTarget;
+    const settings = await getSettings();
+    settings.displayStyle = DISPLAY_STYLE.OPPOSITE[selectorTR.dataset.displayStyle];
+    await saveSettingsAndUpdatePopup(settings);
+    sendMessageToTab({command: COMMAND.UPDATE_PAGE});
+  };
+
+  const sortingSelectionHandler = async event => {
+    const selectorTR = event.currentTarget;
+    const settings = await getSettings();
+    settings.sorting = SORTING.OPPOSITE[selectorTR.dataset.sorting];
+    await saveSettingsAndUpdatePopup(settings);
+  };
+
+  const saveSettingsAndUpdatePopup = async settings => {
+    await saveSettings(settings);
+    updatePopup();
+  };
+
   getById('displaySwitchRow').addEventListener('click', displayStyleSelectionHandler);
   getById('sortingSwitchRow').addEventListener('click', sortingSelectionHandler);
   updatePopup();
-};
-
-const updatePopup = async () => {
-  const settings = await getSettings();
-
-  getById('displaySwitch').src = settings.displayStyle === DISPLAY_STYLE.FADE ? SWITCH_LEFT_IMG_SRC : SWITCH_RIGHT_IMG_SRC;
-  getById('displaySwitchRow').dataset.displayStyle = settings.displayStyle;
-  getById('sortingSwitch').src = settings.sorting === SORTING.DEFAULT ? SWITCH_LEFT_IMG_SRC : SWITCH_RIGHT_IMG_SRC;
-  getById('sortingSwitchRow').dataset.sorting = settings.sorting;
-  getById('blacklist').innerHTML = '';
-
-  if (settings.sorting === SORTING.TITLE) {
-    settings.blacklist.sort(titleComparator);
-  }
-
-  if (settings.blacklist.length > 0) {
-    let prevTitle = null;
-    settings.blacklist.forEach((item, index) => {
-      const titleWithoutArticle = withoutArticle(item.title);
-
-      getById('blacklist').appendChild(tr(
-          td(img({src: SHOW_IMG_SRC, style: 'cursor: pointer;', eventListener: ['click', () => removeFromBlacklist(item.id)]})),
-          td(img({src: item.type === TYPE.TV ? TV_IMG_SRC : FILM_IMG_SRC})),
-          td({
-            style: settings.sorting === SORTING.DEFAULT && index > 0 || settings.sorting === SORTING.TITLE && prevTitle && prevTitle[0] < titleWithoutArticle[0]
-                ? 'border-top: 1px solid lightgray;'
-                : 'border-top: 1px solid white;',
-            innerHTML: settings.sorting === SORTING.TITLE ? withGreyedOutArticle(item.title) : item.title,
-          })));
-
-      prevTitle = titleWithoutArticle;
-    });
-  } else {
-    getById('blacklist').appendChild(tr(td({style: 'color: gray; text-align: center; font-size: 80%;', innerHTML: 'Your blacklist is empty.'})));
-  }
-};
-
-const removeFromBlacklist = async id => {
-  const settings = await getSettings();
-  settings.blacklist = settings.blacklist.filter(item => item.id !== id);
-  await saveSettingsAndUpdatePopup(settings);
-  sendMessageToTab({command: COMMAND.UPDATE_PAGE});
-};
-
-const displayStyleSelectionHandler = async event => {
-  const selectorTR = event.currentTarget;
-  const settings = await getSettings();
-  settings.displayStyle = DISPLAY_STYLE.OPPOSITE[selectorTR.dataset.displayStyle];
-  await saveSettingsAndUpdatePopup(settings);
-  sendMessageToTab({command: COMMAND.UPDATE_PAGE});
-};
-
-const sortingSelectionHandler = async event => {
-  const selectorTR = event.currentTarget;
-  const settings = await getSettings();
-  settings.sorting = SORTING.OPPOSITE[selectorTR.dataset.sorting];
-  await saveSettingsAndUpdatePopup(settings);
-};
-
-const saveSettingsAndUpdatePopup = async settings => {
-  await saveSettings(settings);
-  updatePopup();
-};
-
-initPopup();
+})();
